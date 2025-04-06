f3ebd8a71aa8ac7dcdfe6aa67675389ef611d53c
Brunikito <brunoluiszrosa@gmail.com>
2025-04-06 01:05:29 -0300

    Generated the user data. Currently working on the generate_user function

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Interactive-Media-Evolution-and-Recommendation
 Attempt to generate data while also updating the recommendation algorithm
+
+# Sleep data
+Based on data retrieved from [Sleepcycle](https://sandman.sleepcycle.com/data) on 04/05/2025.
+
+# Country data
+Based on data retrieved from [GeoNames](https://download.geonames.org/export/dump/countryInfo.txt) on 05/05/2025
+
+# bibs
+random-username
--- src/iterate.py
+++ src/iterate.py
@@ -7,8 +7,31 @@ import table_operations
 import initialize
 
 # load the tables
-channels = pd.read_csv('data/channels.csv')
-comments = pd.read_csv('data/comments.csv')
-content = pd.read_csv('data/content.csv')
-users = pd.read_csv('data/users.csv')
+channels = pd.read_csv('../data/channels.csv')
+comments = pd.read_csv('../data/comments.csv')
+content = pd.read_csv('../data/content.csv')
+users = pd.read_csv('../data/users.csv')
+country_data = pd.read_csv('../data/behavior_generated/country_data_cleaned.csv')
+sleep_data = pd.read_csv('../data/behavior_generated/sleep_hours_by_age_country.csv')
+work_data = pd.read_csv('../data/behavior_generated/work_behavior.csv')
 
+'''
+def create_random_user(number_of_users):
+    'user_id', #
+    'user_name', #
+    'user_bed_time', #
+    'user_wake_time', #
+    'user_lunch_time', #
+    'user_dinner_time', #
+    'user_exercise_time', #
+    'user_work_time', #
+    'user_free_from_work_time', #
+    'user_work_days', #
+    'user_age', #
+    'user_gender', #
+    'user_location', #
+    'user_language', #
+    'user_ocupation', #
+    'user_education', #
+    'user_video_watching_time', #
+    'user_video_retention_time', #'''
